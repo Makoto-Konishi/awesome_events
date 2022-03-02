543f85157a62569945721580ac7701fc0bcde8ac
Makoto Konishi <xiaoxishicheng@gmail.com>
2022-03-02 15:30:12 +0900

    eventモデルとeventsコントローラの定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_091259) do
+ActiveRecord::Schema.define(version: 2022_03_02_030542) do
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "owner_id", null: false
+    t.string "name", null: false
+    t.string "place", null: false
+    t.datetime "start_at", null: false
+    t.datetime "end_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_events_on_owner_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false
